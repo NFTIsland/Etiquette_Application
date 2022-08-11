72a3bba9644913a87e2abea215519d3fbacd8173
lspc678 <dltmdqls1945@naver.com>
2022-08-11 22:15:12 +0900

    add routes for metadata, kip17

--- Backend/routes/kasRoutes.js
+++ Backend/routes/kasRoutes.js
@@ -4,4 +4,10 @@ const router = express.Router();
 const walletRouter = require('./walletRoutes');
 router.use('/wallet', walletRouter);
 
+const metadataRouter = require('./metadataRoutes');
+router.use('/metadata', metadataRouter);
+
+const kip17Router = require('./kip17Routes');
+router.use('/kip17', kip17Router);
+
 module.exports = router;
