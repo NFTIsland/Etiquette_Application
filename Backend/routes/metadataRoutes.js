6569d7fed7d3f4dc82e18560c0911f091078d3bf
lspc678 <dltmdqls1945@naver.com>
2022-08-11 22:14:02 +0900

    add metadataRoutes.js

---
+++ Backend/routes/metadataRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const metadataUpload = require('../controller/kas/metadata/metadataUpload');
+const metadataLoad = require('../controller/kas/metadata/metadataLoad');
+
+router.post('/metadataUpload', metadataUpload);
+router.get('/metadataLoad', metadataLoad);
+
+module.exports = router;
