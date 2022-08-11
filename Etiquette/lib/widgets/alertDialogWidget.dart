078eddd5eb1a701cd0ef104dd42991d718014460
Ninelie <sw4968@naver.com>
2022-08-11 17:47:34 +0900

    add displayDialog_register, checkonly functions

--- Etiquette/lib/widgets/alertDialogWidget.dart
+++ Etiquette/lib/widgets/alertDialogWidget.dart
@@ -1,6 +1,5 @@
-import 'package:Etiquette/Login.dart';
+import 'package:Etiquette/Screens/Login.dart';
 import 'package:flutter/material.dart';
-import '../Login.dart';
 
 void displayDialog(context, title, text) => showDialog(
   context: context,
@@ -8,9 +7,16 @@ void displayDialog(context, title, text) => showDialog(
       AlertDialog(title: Text(title), content: Text(text)),
 );
 
-void displayDialog_checkonly(context, title, text) => showDialog(
+void displayDialog_register(context, title, text) => showDialog(
   context: context,
   builder: (context) =>
       AlertDialog(title: Text(title), content: Text(text), actions: <Widget>[TextButton(
           onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login())), child: const Text('OK'),),],),
+);
+
+void displayDialog_checkonly(context, title, text) => showDialog(
+  context: context,
+  builder: (context) =>
+      AlertDialog(title: Text(title), content: Text(text), actions: <Widget>[TextButton(
+        onPressed: () => Navigator.of(context).pop(), child: const Text('OK'),),],),
 );
