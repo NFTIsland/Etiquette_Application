6eee1c2144eca5647a13584639d11edff4540501
lspc678 <dltmdqls1945@naver.com>
2022-08-11 22:13:09 +0900

    add wallet routes for getBalance, transactionRetrieve, klayTransaction

--- Backend/routes/walletRoutes.js
+++ Backend/routes/walletRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const createAccount = require('../controller/kas/wallet/createAccount');
 const checkAccount = require('../controller/kas/wallet/checkAccount');
+const getBalance = require('../controller/kas/wallet/getBalance');
+const transactionRetrieve = require('../controller/kas/wallet/transactionRetrieve');
+const klayTransaction = require('../controller/kas/wallet/klayTransaction');
 
 router.post('/createAccount', createAccount);
 router.get('/checkAccount/:address', checkAccount);
+router.post('/getBalance', getBalance);
+router.get('/transactionRetrieve/:transactionHash', transactionRetrieve);
+router.post('/klayTransaction', klayTransaction);
 
 module.exports = router;
