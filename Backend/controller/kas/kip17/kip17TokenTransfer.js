60d0988c4c53b510ad8348b64bc80f8fcf98c306
lspc678 <dltmdqls1945@naver.com>
2022-08-12 20:44:58 +0900

    kip17TokenTransfer.js add exception code 1000000, 1104801

--- Backend/controller/kas/kip17/kip17TokenTransfer.js
+++ Backend/controller/kas/kip17/kip17TokenTransfer.js
@@ -26,6 +26,9 @@ const kip17TokenTransfer = async function (req, res) {
             res.status(tokenTransfer['statusCode']);
 
             switch (errorCode) {
+                case 1000000:
+                    res.json({statusCode: tokenTransfer['statusCode'], msg: "송신자와 수신자의 주소가 같습니다."});
+                    break;
                 case 1010007:
                     res.json({statusCode: tokenTransfer['statusCode'], msg: "입력한 경로(path)나 동작(method)이 잘못 되었습니다."});
                     break;
@@ -49,6 +52,9 @@ const kip17TokenTransfer = async function (req, res) {
                     break;
                 case 1104700:
                     res.json({statusCode: tokenTransfer['statusCode'], msg: "유효하지 않은 별칭입니다. 제약 사항을 참고해주세요."});
+                    break;
+                case 1104801:
+                    res.json({statusCode: tokenTransfer['statusCode'], msg: "해당 토큰을 전송할 권한이 없습니다."});
                     break;
                 default:
                     res.json({statusCode: tokenTransfer['statusCode'], msg: tokenTransfer['data']['message']});
