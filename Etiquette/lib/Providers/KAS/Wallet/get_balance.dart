15f1a3dc4cb40b2418453681599862678757c606
lspc678 <dltmdqls1945@naver.com>
2022-08-13 15:25:56 +0900

    get_balance.dart

---
+++ Etiquette/lib/Providers/KAS/Wallet/get_balance.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:Etiquette/Models/dio_models.dart';
+import 'package:Etiquette/Models/serverset.dart';
+
+// 잔액 조회
+Future<Map<String, dynamic>> getBalance(String address) async {
+  Dio dio = getBasicDio();
+
+  try {
+    final res = await dio.post("$SERVER_IP/kas/wallet/getBalance", data: {
+      "address": address,
+    });
+    Map<String, dynamic> data = res.data;
+    return data;
+  } on DioError catch (e) {
+    final handleError = e.response?.data; // {statusCode: 400, msg: 주소가 잘못되었습니다.}
+    if (handleError == null) {
+      Map<String, dynamic> data = {
+        "statusCode": 400,
+        "msg": "서버와의 연결이 원활하지 않습니다.",
+      };
+      return data;
+    } else {
+      Map<String, dynamic> data = {
+        "statusCode": e.response?.statusCode,
+        "msg": handleError['msg'],
+      };
+      return data;
+    }
+  } catch (ex) {
+    Map<String, dynamic> data = {
+      "statusCode": 400,
+      "msg": ex.toString()
+    };
+    return data;
+  }
+}
