370fa95cef2abd6eac42aafbca764138887e7c84
lspc678 <dltmdqls1945@naver.com>
2022-08-19 16:32:27 +0900

    add displayDialog_YesOrNo

--- Etiquette/lib/widgets/alertDialogWidget.dart
+++ Etiquette/lib/widgets/alertDialogWidget.dart
@@ -20,3 +20,23 @@ void displayDialog_checkonly(context, title, text) => showDialog(
       AlertDialog(title: Text(title), content: Text(text), actions: <Widget>[TextButton(
         onPressed: () => Navigator.of(context).pop(), child: const Text('OK'),),],),
 );
+
+Future<bool> displayDialog_YesOrNo(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        TextButton(
+          child: const Text('OK'),
+          onPressed: () => Navigator.pop(context, true),
+        ),
+      ],
+    ),
+  );
+}
