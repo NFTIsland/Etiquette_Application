0b5f18cf45f4ef459ec4ed6c6f1673cb10cfc3a0
lspc678 <dltmdqls1945@naver.com>
2022-08-24 23:00:23 +0900

    new Providers file get_ticket_seat_image_url.dart

---
+++ Etiquette/lib/Providers/DB/get_ticket_seat_image_url.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:Etiquette/Models/serverset.dart';
+
+Future<Map<String, dynamic>> getTicketSeatImageUrl(String product_name, String place) async {
+  try {
+    final url = "$SERVER_IP/ticketSeatImageUrl/$product_name/$place";
+    final res = await http.get(Uri.parse(url));
+    Map<String, dynamic> data = json.decode(res.body);
+    return data;
+  } catch (ex) {
+    Map<String, dynamic> data = {
+      "statusCode": 400,
+      "msg": ex.toString()
+    };
+    return data;
+  }
+}
