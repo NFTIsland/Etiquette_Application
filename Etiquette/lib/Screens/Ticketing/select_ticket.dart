cc5a5825a0d524f05feff46a563474cf04d05266
Ninelie <sw4968@naver.com>
2022-08-24 23:41:36 +0900

    Add function to pay for selected ticket

--- Etiquette/lib/Screens/Ticketing/select_ticket.dart
+++ Etiquette/lib/Screens/Ticketing/select_ticket.dart
@@ -4,17 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:Etiquette/widgets/appbar.dart';
+import 'package:Etiquette/Widgets/alertDialogWidget.dart';
 import 'package:Etiquette/Models/serverset.dart';
 import 'package:Etiquette/Utilities/add_comma_to_number.dart';
 import 'package:Etiquette/Utilities/round.dart';
 import 'package:Etiquette/Providers/Coinone/get_klay_currency.dart';
 import 'package:Etiquette/Providers/DB/get_ticket_seat_image_url.dart';
+import 'package:Etiquette/Providers/DB/get_kas_address.dart';
+import 'package:Etiquette/Providers/DB/update_ticket_owner.dart';
+import 'package:Etiquette/Providers/KAS/Kip17/kip17_token_transfer.dart';
+import 'package:Etiquette/Providers/KAS/Wallet/klay_transaction.dart';
 
 class SelectTicket extends StatefulWidget {
   String? product_name;
   String? place;
+  String? category;
 
-  SelectTicket({Key? key, this.product_name, this.place}) : super(key: key);
+  SelectTicket({Key? key, this.product_name, this.place, this.category}) : super(key: key);
 
   @override
   State createState() => _SelectTicket();
@@ -33,7 +39,7 @@ class _SelectTicket extends State<SelectTicket> {
   // String image_url = "https://firebasestorage.googleapis.com/v0/b/island-96845.appspot.com/o/seat_image%2Fseoul_a_theater.jpg?alt=media&token=9e93c0ac-960c-4853-916f-731218502647";
   String image_url = "";
 
-  double klayCurrency = 0.0;
+  double _klayCurrency = 0.0;
 
   List _performanceDate = new List.empty(growable: true);
   String date_value = "예매 일자 선택";
@@ -236,7 +242,7 @@ class _SelectTicket extends State<SelectTicket> {
             _price = ticket_price.toString();
           });
           _payInfo = "최종 결제 금액: ${_price.replaceAllMapped(reg, mathFunc)}원";
-          _klayInfo = "(약 ${klayRound(ticket_price / klayCurrency)} KLAY)";
+          _klayInfo = "(약 ${klayRound(ticket_price / _klayCurrency)} KLAY)";
         } else {
           setState(() {
             _price = "";
@@ -251,10 +257,10 @@ class _SelectTicket extends State<SelectTicket> {
   Future<void> loadKlayCurrency() async {
     Map<String, dynamic> data = await getKlayCurrency(); // 현재 KLAY 시세 정보를 API를 통해 가져옴
     if (data["statusCode"] == 200) { // 현재 KLAY 시세 정보를 정상적으로 가져옴
-      String _klayCurrency = data['lastCurrency'];
-      klayCurrency = double.parse(_klayCurrency);
+      String klayCurrency = data['lastCurrency'];
+      _klayCurrency = double.parse(klayCurrency);
     } else {
-      klayCurrency = 0.0;
+      _klayCurrency = 0.0;
     }
   }
 
@@ -268,6 +274,31 @@ class _SelectTicket extends State<SelectTicket> {
       image_url = data['data'][0]['seat_image_url'];
     } else {
       image_url = "";
+    }
+  }
+
+  Future<Map<String, dynamic>> loadTicketTokenIdAndOwner(String date_value, String time_value, String seat_class_value, String seat_no_value) async {
+    final url = "$SERVER_IP/ticketTokenIdAndOwner/${widget.product_name!}/${widget.place!}/${date_value}/${time_value}/${seat_class_value}/${seat_no_value}";
+    try {
+      var res = await http.get(Uri.parse(url));
+      Map<String, dynamic> data = json.decode(res.body);
+      if (res.statusCode == 200) {
+        return {
+          "token_id": data['data'][0]['token_id'],
+          "owner": data['data'][0]['owner']
+        };
+      } else {
+        return {
+          "token_id": "",
+          "owner": "",
+        };
+      }
+    } catch (ex) {
+      print("티켓 소유자 가져오기 --> ${ex.toString()}");
+      return {
+        "token_id": "",
+        "owner": "",
+      };
     }
   }
 
@@ -659,13 +690,86 @@ class _SelectTicket extends State<SelectTicket> {
                             ),
                             style: ElevatedButton.styleFrom(
                               padding: const EdgeInsets.all(10.0),
-                              backgroundColor: Colors.blue,
+                              onPrimary: Colors.blue,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)
+                                  borderRadius: BorderRadius.circular(50)
                               ),
                             ),
-                            onPressed: () {
+                            onPressed: () async {
+                              if (date_value != "예매 일자 선택"
+                                  && time_value != "예매 시각 선택"
+                                  && seat_class_value != "좌석 등급 선택"
+                                  && seat_no_value != "좌석 번호 선택") {
+                                final pay_selected = await displayDialog_YesOrNo(context, "티켓 결제", "위 옵션으로 결제하시겠습니까?");
+                                if (pay_selected) { // 다이얼 로그에서 OK 누름
+                                  final kas_address_data = await getKasAddress(); // jwt token으로부터 kas_address 가져오기
+                                  final tokenId_and_owner = await loadTicketTokenIdAndOwner(date_value, time_value, seat_class_value, seat_no_value); // 5가지 데이터로 token_id와 owner 가져오기
 
+                                  if (kas_address_data['statusCode'] == 200) {
+                                    if (tokenId_and_owner['owner'] != "" && tokenId_and_owner['token_id'] != "") {
+                                      final sender = kas_address_data['data'][0]['kas_address'];
+                                      final owner = tokenId_and_owner['owner'];
+                                      final token_id = tokenId_and_owner['token_id'];
+
+                                      double? parse_price = double.tryParse(_price);
+                                      if (parse_price != null) {
+                                        double payment_klay = parse_price / _klayCurrency;
+
+                                        if (payment_klay.isFinite) {
+                                          Map<String, dynamic> klayTransactionData = await klayTransaction(sender, payment_klay.toString(), owner);
+
+                                          if (klayTransactionData['statusCode'] == 200) { // 트랜잭션 성공
+                                            Map<String, dynamic> kip17TokenTransferData = await kip17TokenTransfer(widget.category!, token_id, owner, owner, sender);
+
+                                            if (kip17TokenTransferData['statusCode'] == 200) {
+                                              Map<String, dynamic> updateTicketOwnerData = await updateTicketOwner(sender, token_id);
+
+                                              if (updateTicketOwnerData['statusCode'] == 200) {
+                                                displayDialog_checkonly(context, "티켓 결제", "결제가 성공적으로 완료되었습니다.");
+                                              } else { // DB에 티켓 owner를 업데이트 하지 못함
+                                                String errorMessage = "티켓 결제에 실패했습니다.\n\n서버와의 통신이 원활하지 않습니다.";
+                                                displayDialog_checkonly(context, "티켓 결제", errorMessage);
+                                              }
+                                            } else { // 토큰 전송 실패
+                                              String message = kip17TokenTransferData["msg"];
+                                              String errorMessage = "티켓 결제에 실패했습니다.\n\n$message";
+                                              displayDialog_checkonly(context, "티켓 결제", errorMessage);
+
+                                              // 토큰 전송 실패로 인한 klay 환불 조치
+                                              // 문제점 1. owner의 klay 잔액이 부족하면 환불이 진행되지 않는다.
+                                              payment_klay = payment_klay + 0.000525;
+                                              Map<String, dynamic> klayTransactionData = await klayTransaction(owner, payment_klay.toString(), sender);
+                                              if (klayTransactionData['statusCode'] == 200) {
+                                                displayDialog_checkonly(context, "티켓 결제", "알 수 없는 오류로 거래가 취소되었습니다.\n\n다시 시도해 주십시오.");
+                                              } else {
+                                                displayDialog_checkonly(context, "티켓 결제", "알 수 없는 오류로 거래가 취소되었습니다.\n\n서비스 센터에 문의해 주십시오.");
+                                              }
+                                            }
+                                          } else { // 트랜잭션 실패
+                                            String message = klayTransactionData["msg"];
+                                            String errorMessage = "티켓 결제에 실패했습니다.\n\n$message";
+                                            displayDialog_checkonly(context, "티켓 결제", errorMessage);
+                                          }
+                                        } else { // _klayCurrency가 0일 경우
+                                          String errorMessage = "티켓 결제에 실패했습니다.\n\nKLAY 환율 정보를 받아오지 못했습니다.";
+                                          displayDialog_checkonly(context, "티켓 결제", errorMessage);
+                                        }
+                                      } else { // _price를 숫자로 변환하지 못함
+                                        String errorMessage = "티켓 결제에 실패했습니다.\n\n가격 정보를 받아오지 못했습니다.";
+                                        displayDialog_checkonly(context, "티켓 결제", errorMessage);
+                                      }
+                                    } else { // token_id와 owner 주소를 가져오지 못함
+                                      displayDialog_checkonly(context, "티켓 결제", "이미 결제가 완료된 티켓입니다.");
+                                    }
+                                  } else { // jwt token으로부터 kas_address를 가져오지 못했을 경우
+                                    String? message = kas_address_data["msg"];
+                                    String errorMessage = "잔액 정보를 가져오지 못했습니다.\n\n$message";
+                                    displayDialog_checkonly(context, "통신 오류", errorMessage);
+                                  }
+                                }
+                              } else {
+                                displayDialog_checkonly(context, "티켓 선택", "옵션을 모두 선택해 주세요.");
+                              }
                             },
                           ),
                         ),
