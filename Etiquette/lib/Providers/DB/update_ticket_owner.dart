25ee66a07bdda661e6c6a48415ef00c0ef97372a
Ninelie <sw4968@naver.com>
2022-08-24 23:46:53 +0900

    Add function to update ticket owner in DB

---
+++ Etiquette/lib/Providers/DB/update_ticket_owner.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:Etiquette/Models/serverset.dart';
+
+Future<Map<String, dynamic>> updateTicketOwner(String owner, String token_id) async {
+  try {
+    const url = "$SERVER_IP/updateTicketOwner";
+    final res = await http.post(Uri.parse(url), body: {
+      "owner": owner,
+      "token_id": token_id,
+    });
+    Map<String, dynamic> data = json.decode(res.body);
+    return data;
+  } catch (ex) {
+    Map<String, dynamic> data = {
+      "statusCode": 400,
+      "msg": ex.toString()
+    };
+    return data;
+  }
+}
