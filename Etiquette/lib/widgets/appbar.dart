4a9beff09d5b85249eeba9f76b4abc2577a7933e
lspc678 <dltmdqls1945@naver.com>
2022-08-24 22:58:40 +0900

    appbar.dart add appbarWithArrowBackButton()

--- Etiquette/lib/widgets/appbar.dart
+++ Etiquette/lib/widgets/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 @override
 AppBar defaultAppbar(String title) {
@@ -7,5 +8,29 @@ AppBar defaultAppbar(String title) {
     backgroundColor: Colors.white24,
     foregroundColor: Colors.black,
     elevation: 0,
+  );
+}
+
+@override
+AppBar appbarWithArrowBackButton(String title) {
+  return AppBar(
+    title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 25
+        )
+    ),
+    backgroundColor: Colors.white24,
+    foregroundColor: Colors.black,
+    elevation: 0,
+    centerTitle: true,
+    leading: IconButton(
+      onPressed: () {
+        Get.back();
+      },
+      icon: const Icon(
+        Icons.arrow_back_ios_new_rounded
+      )
+    ),
   );
 }
