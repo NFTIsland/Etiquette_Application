96a29b97f012a7f7e35da809235da3eea9f45017
lspc678 <dltmdqls1945@naver.com>
2022-08-24 22:56:57 +0900

    new Providers file get_kas_address.dart

---
+++ Etiquette/lib/Providers/DB/get_kas_address.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:Etiquette/Models/serverset.dart';
+import 'package:Etiquette/Utilities/jwt_decode.dart';
+
+Future<Map<String, dynamic>> getKasAddress() async {
+  try {
+    String id = await jwtDecode();
+    const url = "$SERVER_IP/kas_address";
+    final res = await http.post(Uri.parse(url), body: {
+      "id": id,
+    });
+    Map<String, dynamic> data = json.decode(res.body);
+    return data;
+  } catch (ex) {
+    Map<String, dynamic> data = {
+      "statusCode": 400,
+      "msg": ex.toString()
+    };
+    return data;
+  }
+}
