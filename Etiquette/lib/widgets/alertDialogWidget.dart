14142f6e8d081fc51df19ea46b4930e9a78b756c
lspc678 <dltmdqls1945@naver.com>
2022-08-27 16:29:35 +0900

    alertDialogWidget.dart modify displayDialog, displayDialog_checkonly add await

--- Etiquette/lib/widgets/alertDialogWidget.dart
+++ Etiquette/lib/widgets/alertDialogWidget.dart
@@ -1,25 +1,46 @@
-import 'package:Etiquette/Screens/Login.dart';
 import 'package:flutter/material.dart';
 
-void displayDialog(context, title, text) => showDialog(
-  context: context,
-  builder: (context) =>
-      AlertDialog(title: Text(title), content: Text(text)),
-);
+Future<void> displayDialog(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text)
+    ),
+  );
+}
 
-void displayDialog_register(context, title, text) => showDialog(
-  context: context,
-  builder: (context) =>
-      AlertDialog(title: Text(title), content: Text(text), actions: <Widget>[TextButton(
-          onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login())), child: const Text('OK'),),],),
-);
+// Future<void> displayDialog_register(context, title, text) async {
+//   return await showDialog(
+//     context: context,
+//     builder: (context) => AlertDialog(
+//       title: Text(title),
+//       content: Text(text),
+//       actions: <Widget> [
+//         TextButton(
+//           onPressed: () => Navigator.pop(context),
+//           child: const Text('OK'),
+//         ),
+//       ],
+//     ),
+//   );
+// }
 
-void displayDialog_checkonly(context, title, text) => showDialog(
-  context: context,
-  builder: (context) =>
-      AlertDialog(title: Text(title), content: Text(text), actions: <Widget>[TextButton(
-        onPressed: () => Navigator.of(context).pop(), child: const Text('OK'),),],),
-);
+Future<void> displayDialog_checkonly(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text),
+      actions: <Widget> [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('OK'),
+        ),
+      ],
+    ),
+  );
+}
 
 Future<bool> displayDialog_YesOrNo(context, title, text) async {
   return await showDialog(
