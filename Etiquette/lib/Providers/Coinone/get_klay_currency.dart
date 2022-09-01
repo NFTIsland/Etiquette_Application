26a71978773cc799597cabb09ebe4c5fdc485e4c
lspc678 <dltmdqls1945@naver.com>
2022-09-02 00:08:13 +0900

    replace dio with http

--- Etiquette/lib/Providers/Coinone/get_klay_currency.dart
+++ Etiquette/lib/Providers/Coinone/get_klay_currency.dart
@@ -1,12 +1,11 @@
-import 'package:dio/dio.dart';
-import 'package:Etiquette/Models/dio_models.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 // 실시간 KLAY 시세 정보
 Future<Map<String, dynamic>> getKlayCurrency() async {
-  Dio dio = getBasicDio();
   try {
-    final res = await dio.get("https://api.coinone.co.kr/public/v2/ticker_new/KRW/KLAY");
-    Map<String, dynamic> data = res.data;
+    final res = await http.get(Uri.parse("https://api.coinone.co.kr/public/v2/ticker_new/KRW/KLAY"));
+    Map<String, dynamic> data = json.decode(res.body);
     if (data["result"] == "success") {
       final lastCurrency = data["tickers"][0]["last"];
       return {
@@ -18,21 +17,6 @@ Future<Map<String, dynamic>> getKlayCurrency() async {
         "statusCode": 400,
         "msg": "KLAY 시세 정보를 불러오는데 실패했습니다."
       };
-    }
-  } on DioError catch (e) {
-    final handleError = e.response?.data;
-    if (handleError == null) {
-      Map<String, dynamic> data = {
-        "statusCode": 404,
-        "msg": "서버와의 연결이 원활하지 않습니다.",
-      };
-      return data;
-    } else {
-      Map<String, dynamic> data = {
-        "statusCode": e.response?.statusCode,
-        "msg": handleError['msg'],
-      };
-      return data;
     }
   } catch (ex) {
     Map<String, dynamic> data = {
