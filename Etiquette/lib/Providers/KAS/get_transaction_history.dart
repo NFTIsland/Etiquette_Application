8cacca8619ee2f5aec842733d04ae39f272b641b
lspc678 <dltmdqls1945@naver.com>
2022-09-24 01:21:27 +0900

    get_transaction_history.dart delete size parameter and add type, period

--- Etiquette/lib/Providers/KAS/get_transaction_history.dart
+++ Etiquette/lib/Providers/KAS/get_transaction_history.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:Etiquette/Models/serverset.dart';
 
-Future<Map<String, dynamic>> getTransactionHistory(String kas_address, int size) async {
-  // final url = 'https://th-api.klaytnapi.com/v2/transfer/account/$kas_address?kind=klay,nft&size=$size&exclude-zero-klay=true';
+Future<Map<String, dynamic>> getTransactionHistory(String kas_address, String type, String period) async {
   const url = "$SERVER_IP/kas/wallet/transactionHistory";
   try {
     final res = await http.post(Uri.parse(url), body: {
       "address": kas_address,
-      "size": size.toString(),
+      "type": type,
+      "period": period,
     });
     Map<String, dynamic> data = json.decode(res.body);
     return data;
