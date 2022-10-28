ca771f085911a8b3f12aa7f3dff3dfe5f83258dd
Ryumaker <sw4968@naver.com>
2022-10-28 12:58:21 +0900

    Add 'displayDialog_checkonly_directNN', 'displayDialog_checkonly_changeNickname', 'displayDialog_changeIndividual' for smooth page route movement

--- Etiquette/lib/widgets/alertDialogWidget.dart
+++ Etiquette/lib/widgets/alertDialogWidget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:Etiquette/Screens/Drawer/ChangeUserInfo.dart';
+import 'package:Etiquette/Screens/Drawer/Change_nickname.dart';
 
 Future<void> displayDialog(context, title, text) async {
   return await showDialog(
@@ -26,6 +29,39 @@ Future<void> displayDialog_checkonly(context, title, text) async {
   );
 }
 
+Future<void> displayDialog_checkonly_directNN(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text),
+      actions: <Widget> [
+        TextButton(
+          onPressed: () => {Navigator.popUntil(context, ModalRoute.withName('/ChangeNickname'))},
+          child: const Text('OK'),
+        ),
+      ],
+    ),
+  );
+}
+
+Future<void> displayDialog_checkonly_changeNickname(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text),
+      actions: <Widget> [
+        TextButton(
+          onPressed: () => {
+            Get.to(() => ChangeUserInfo())},
+          child: const Text('OK'),
+        ),
+      ],
+    ),
+  );
+}
+
 Future<bool> displayDialog_YesOrNo(context, title, text) async {
   return await showDialog(
     context: context,
@@ -40,6 +76,22 @@ Future<bool> displayDialog_YesOrNo(context, title, text) async {
         TextButton(
           child: const Text('OK'),
           onPressed: () => Navigator.pop(context, true),
+        ),
+      ],
+    ),
+  );
+}
+
+Future<void> displayDialog_changeIndividual(context, title, text) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(text),
+      actions: <Widget> [
+        TextButton(
+          onPressed: () {Get.to(() => ChangeUserInfo());},
+          child: const Text('OK'),
         ),
       ],
     ),
