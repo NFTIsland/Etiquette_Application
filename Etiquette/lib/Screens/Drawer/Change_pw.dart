3536cd658da69c39404dbb5ab49b937a97ab153f
Ryumaker <sw4968@naver.com>
2022-10-28 09:53:00 +0900

    Make the case of errors more concrete and Clean up unnecessary variables

--- Etiquette/lib/Screens/Drawer/Change_pw.dart
+++ Etiquette/lib/Screens/Drawer/Change_pw.dart
@@ -1,50 +1,75 @@
+import 'dart:convert';
+import 'package:Etiquette/widgets/alertDialogWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:Etiquette/Models/serverset.dart';
+import 'package:Etiquette/Utilities/jwt_decode.dart';
+
 class ChangePW extends StatefulWidget {
   State createState() => _ChangePW();
 }
 
 class _ChangePW extends State<ChangePW> {
-  Color act = Colors.grey;
-  Color pwc = Colors.grey.shade200;
   Color chc = Colors.grey.shade200;
   Color rec = Colors.grey.shade200;
-  late bool chk = false;
-  bool cuch = false;
-  bool chch = false;
-  bool rech = false;
   String chpw = "";
   String repw = "";
-  final _formkey_cur = GlobalKey<FormState>();
   final _formkey_cha = GlobalKey<FormState>();
   final _formkey_re = GlobalKey<FormState>();
   final cha = TextEditingController();
   final re = TextEditingController();
-  FocusNode _pwtextFieldFocus = FocusNode();
-  FocusNode _chtextFieldFocus = FocusNode();
-  FocusNode _retextFieldFocus = FocusNode();
+  final FocusNode _chtextFieldFocus = FocusNode();
+  final FocusNode _retextFieldFocus = FocusNode();
+  String? id = "";
+  String? nickname = "";
 
-  void acti() {
-    _formkey_cha.currentState!.save();
-    Get.back();
-    // Navigator.pop(context);
+  Future<void> getInfo() async {
+    id = await jwtDecode();
+    nickname = await storage.read(key: "nickname");
+  }
+
+  Future<bool> check_curPW(String id, String pw) async {
+    try {
+      const url = "$SERVER_IP/auth/checkPassword";
+      final res = await http.post(Uri.parse(url), body: {"id": id, "pw": pw});
+      Map<String, dynamic> data = json.decode(res.body);
+      if (data['statusCode'] == 200) {
+        return true;
+      }
+    } catch (ex) {
+      return false;
+    }
+    return false;
+  }
+
+  Future<int> updatePW(String id, String pw) async {
+    if (pw.length < 8) {
+      return 1;
+    } else if (await check_curPW(id, pw) == true) {
+      return 2;
+    } else {
+      try {
+        const url = "$SERVER_IP/auth/updatePassword";
+        final res = await http.post(Uri.parse(url), body: {
+          "id": id,
+          "pw": pw,
+        });
+        Map<String, dynamic> data = json.decode(res.body);
+        if (data['statusCode'] == 200) {
+          return 0;
+        }
+      } catch (ex) {
+        return 3;
+      }
+    }
+    return 3;
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _pwtextFieldFocus.addListener(() {
-      if (_pwtextFieldFocus.hasFocus) {
-        setState(() {
-          pwc = Colors.white24;
-        });
-      } else {
-        setState(() {
-          pwc = Colors.grey.shade200;
-        });
-      }
-    });
+    getInfo();
     _chtextFieldFocus.addListener(() {
       if (_chtextFieldFocus.hasFocus) {
         setState(() {
@@ -77,185 +102,77 @@ class _ChangePW extends State<ChangePW> {
         },
         child: Scaffold(
             appBar: AppBar(
-              title: Text("비밀번호 변경",
+              title: const Text("비밀번호 변경",
                   style: TextStyle(fontWeight: FontWeight.bold)),
               elevation: 0,
               foregroundColor: Colors.black,
               backgroundColor: Colors.white24,
               automaticallyImplyLeading: false,
               leading: IconButton(
-                icon: Icon(Icons.arrow_back_ios_new_rounded),
+                icon: const Icon(Icons.arrow_back_ios_new_rounded),
                 onPressed: () {
-                  Get.back();
-                  //Navigator.pop(context);
+                  Get.back(); //Navigator.pop(context);
                 },
               ),
-              actions: <Widget>[
-                TextButton(
-                    child:
-                        Text("완료", style: TextStyle(color: act, fontSize: 20)),
-                    onPressed: () {
-                      chk ? acti() : null;
-                    },
-                    style: TextButton.styleFrom(
-                        splashFactory: NoSplash.splashFactory)
-                    //splashColor: Colors.transparent,
-                    //highlightColor: Colors.transparent,
-                    )
-              ],
             ),
             body: SingleChildScrollView(
                 child: Column(children: <Widget>[
-              SizedBox(height: 20),
-              Column(children: <Widget>[
-                Text("현재 비밀번호 ",
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                SizedBox(height: 10),
-                Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("현재 비밀번호",
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Form(
-                            key: _formkey_cur,
-                            child: TextFormField(
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              obscureText: true,
-                              keyboardType: TextInputType.text,
-                              //기본으로 자판 모양의 키보드 호출되도록 설정
-                              decoration: InputDecoration(
-                                //icon: const Text("ID:"),
-                                //labelText: "ID", //입력칸에 ID 표시되도록
-                                filled: true,
-                                fillColor: pwc,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5),
-                                    borderSide: BorderSide(
-                                      width: 0,
-                                    )),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5),
-                                    borderSide: BorderSide(
-                                        width: 2, color: Color(0xffFFB877))),
-                              ),
-                              focusNode: _pwtextFieldFocus,
-                              onSaved: (text) {
-                                setState(() {
-                                  chpw = text
-                                      as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
-                                });
-                              },
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return "Please enter Current Password";
-                                }
-                                return null;
-                              },
-                              onChanged: (text) {
-                                if (_formkey_cur.currentState!.validate()) {
-                                  setState(() {
-                                    cuch = true;
-                                    if (chch == true && rech == true) {
-                                      chk = true;
-                                      act = Colors.black;
-                                    }
-                                  });
-                                } else {
-                                  setState(() {
-                                    cuch = false;
-                                    chk = false;
-                                    act = Colors.grey;
-                                  });
-                                }
-                              },
-                            ),
-                          ),
-                        ])),
-              ]),
+              const SizedBox(height: 20),
               Padding(
-                  padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("새 비밀번호",
+                        const Text("새 비밀번호",
                             style: TextStyle(fontWeight: FontWeight.bold)),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
                         Form(
                           key: _formkey_cha,
                           child: TextFormField(
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
                             obscureText: true,
                             keyboardType: TextInputType.text,
-                            controller: cha,
-                            //기본으로 자판 모양의 키보드 호출되도록 설정
+                            controller: cha, //기본으로 자판 모양의 키보드 호출되도록 설정
                             decoration: InputDecoration(
-                              //icon: const Text("ID:"),
-                              //labelText: "ID", //입력칸에 ID 표시되도록
                               filled: true,
                               fillColor: chc,
                               border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(5),
-                                  borderSide: BorderSide(
+                                  borderSide: const BorderSide(
                                     width: 0,
                                   )),
                               focusedBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(5),
-                                  borderSide: BorderSide(
+                                  borderSide: const BorderSide(
                                       width: 2, color: Color(0xffFFB877))),
                             ),
                             focusNode: _chtextFieldFocus,
                             onSaved: (text) {
                               setState(() {
-                                repw = text
-                                    as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
+                                chpw = text as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
                               });
                             },
                             validator: (value) {
                               if (value == null || value.isEmpty) {
                                 return "Please enter Password for change";
+                              } else if (value.length < 8) {
+                                return "Password must be at least 8 characters long";
                               }
                               return null;
-                            },
-                            onChanged: (text) {
-                              if (_formkey_cha.currentState!.validate() &&
-                                  _formkey_re.currentState!.validate()) {
-                                setState(() {
-                                  chch = true;
-                                  rech = true;
-                                  if (cuch == true && rech == true) {
-                                    chk = true;
-                                    act = Colors.black;
-                                  }
-                                });
-                              } else {
-                                setState(() {
-                                  chch = false;
-                                  chk = false;
-                                  act = Colors.grey;
-                                });
-                              }
                             },
                           ),
                         ),
                       ])),
               Padding(
-                  padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("새 비밀번호 확인",
+                        const Text("새 비밀번호 확인",
                             style: TextStyle(fontWeight: FontWeight.bold)),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
                         Form(
@@ -268,57 +185,59 @@ class _ChangePW extends State<ChangePW> {
                             controller: re,
                             //기본으로 자판 모양의 키보드 호출되도록 설정
                             decoration: InputDecoration(
-                              //icon: const Text("ID:"),
-                              //labelText: "ID", //입력칸에 ID 표시되도록
                               filled: true,
                               fillColor: rec,
                               border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(5),
-                                  borderSide: BorderSide(
+                                  borderSide: const BorderSide(
                                     width: 0,
                                   )),
                               focusedBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(5),
-                                  borderSide: BorderSide(
+                                  borderSide: const BorderSide(
                                       width: 2, color: Color(0xffFFB877))),
                             ),
                             focusNode: _retextFieldFocus,
                             onSaved: (text) {
                               setState(() {
-                                repw = text
-                                    as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
+                                repw = text as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
                               });
                             },
                             validator: (value) {
                               if (value == null || value.isEmpty) {
                                 return "Please enter PW for check";
-                              }
-                              if (cha.text != re.text) {
+                              } else if (cha.text != re.text) {
                                 return "비밀번호가 일치하지 않습니다.";
                               }
                               return null;
                             },
-                            onChanged: (text) {
-                              if (_formkey_re.currentState!.validate() &&
-                                  _formkey_cha.currentState!.validate()) {
-                                setState(() {
-                                  rech = true;
-                                  chch = true;
-                                  if (cuch == true && chch == true) {
-                                    chk = true;
-                                    act = Colors.black;
-                                  }
-                                });
-                              } else {
-                                setState(() {
-                                  rech = false;
-                                  chk = false;
-                                  act = Colors.grey;
-                                });
-                              }
-                            },
                           ),
                         ),
+                        Center(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
+                              onPressed: () async {
+                                if (cha.value.text == re.value.text) {
+                                  if (await updatePW(id!, cha.value.text) == 0) {
+                                    displayDialog_changeIndividual(context, "비밀번호 변경 성공", "비밀번호를 성공적으로 변경했습니다.");
+                                  }
+                                  else if (await updatePW(id!, cha.value.text) == 1) {
+                                    displayDialog_checkonly(context, "비밀번호 변경 오류", "비밀번호는 최소 8글자 이상이어야 합니다.");
+                                  }
+                                  else if (await updatePW(id!, cha.value.text) == 2) {
+                                    displayDialog_checkonly(context, "기존 비밀번호 재사용", "기존과 같은 비밀번호로는 변경할 수 없습니다.\n다시 시도해주십시오.");
+                                  }
+                                  else if (await updatePW(id!, cha.value.text) == 3) {
+                                    displayDialog_checkonly(context, "비밀번호 변경 오류", "오류로 인하여 비밀번호 변경에 실패했습니다.\n다시 시도해주십시오.");
+                                  }
+                                }
+                                else {
+                                  displayDialog_checkonly(context, "새 비밀번호 확인 오류", "새 비밀번호 확인을 위해 다시 한번 입력해주십시오.");
+                                }
+                              },
+                              child: const Text("비밀번호 변경",
+                                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))),
+                        )
                       ]))
             ]))));
   }
