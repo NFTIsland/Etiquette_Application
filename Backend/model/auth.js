dbef7d4ac1ab28ac59450788526218288a36036e
Ryumaker <sw4968@naver.com>
2022-10-28 12:54:14 +0900

    Add SQL to check and update password and nickname

--- Backend/model/auth.js
+++ Backend/model/auth.js
@@ -7,13 +7,22 @@ module.exports = {
         connection.query("SELECT * FROM user_db.users WHERE id = ? AND pw = ?;", [id, cryptPassword], callback);
     },
 
-    selectSignUp: function (id, pw, callback) {
+    selectSignUp: function (id, callback) {
         connection.query("SELECT * FROM users WHERE id = (?);", [id], callback);
     },
 
     insert: function (id, pw, nickname, kas_address, callback) {
         var cryptPassword = crypto.createHash('sha256').update(pw).digest('hex');
         connection.query("INSERT INTO user_db.users (id, pw, nickname, kas_address, role) VALUES (?, ?, ?, ?, ?);", [id, cryptPassword, nickname, kas_address, '2'], callback);
+    },
+
+    updatePW: function (id, pw, callback) {
+        var cryptPassword = crypto.createHash('sha256').update(pw).digest('hex');
+        connection.query("UPDATE user_db.users SET pw = ? WHERE id = ?;", [cryptPassword, id], callback);
+    },
+
+    updateNickName: function (id, nickname, callback) {
+        connection.query("UPDATE user_db.users SET nickname = ? WHERE id = ?;", [nickname, id], callback);
     },
 
     selectNickname: function (nickname, callback) {
