1be7c6e76d8093f95e46b671d89ea8a3c72892c1
Ryumaker <sw4968@naver.com>
2022-10-28 12:55:29 +0900

    Add routes for checking and updating password and nickname

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,7 +5,10 @@ const authController = require("../controller/authController");
 router.post('/login', authController.login);
 router.get('/data', authController.home);
 router.post('/signup', authController.signup);
+router.post('/updatePassword', authController.updatePassword);
+router.post('/updateNickname', authController.updateNickname);
 router.post('/checkNickname', authController.checkNickname);
+router.post('/checkPassword', authController.checkPassword);
 router.post('/kas_address', authController.kasAddress);
 
 module.exports = router;
