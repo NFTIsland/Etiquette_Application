ce07dd865d2ef0f58543ce239ae2aa26d0c5976a
Ryumaker <sw4968@naver.com>
2022-10-28 09:09:47 +0900

    Add Page for checking current password before users change pw

---
+++ Etiquette/lib/Screens/Drawer/Check_CurPW.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+import 'package:Etiquette/widgets/alertDialogWidget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:Etiquette/Models/serverset.dart';
+import 'package:Etiquette/Utilities/jwt_decode.dart';
+import 'package:Etiquette/Screens/Drawer/Change_pw.dart';
+
+class checkcurPW extends StatefulWidget {
+  State createState() => _checkcurPW();
+}
+
+class _checkcurPW extends State<checkcurPW> {
+  Color pwc = Colors.grey.shade200;
+  bool check = false;
+  String curpw = "";
+  final _formkey_cur = GlobalKey<FormState>();
+  final cur = TextEditingController();
+  FocusNode _pwtextFieldFocus = FocusNode();
+  String? id = "";
+  String? nickname = "";
+
+  Future<void> getInfo() async {
+    id = await jwtDecode();
+    nickname = await storage.read(key: "nickname");
+  }
+
+  Future<bool> check_curPW(String id, String pw) async{
+    try {
+      const url = "$SERVER_IP/auth/checkPassword";
+      final res = await http.post(Uri.parse(url), body: {
+        "id": id,
+        "pw": pw
+      });
+      Map<String, dynamic> data = json.decode(res.body);
+      if (data['statusCode'] == 200){
+        return true;
+      }
+    } catch (ex) {
+      return false;
+    }
+    return false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getInfo();
+    _pwtextFieldFocus.addListener(() {
+      if (_pwtextFieldFocus.hasFocus) {
+        setState(() {
+          pwc = Colors.white24;
+        });
+      } else {
+        setState(() {
+          pwc = Colors.grey.shade200;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text("비밀번호 변경",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              elevation: 0,
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white24,
+              automaticallyImplyLeading: false,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                onPressed: () {
+                  Get.back();
+                },
+              ),
+            ),
+            body: SingleChildScrollView(
+                child: Column(children: <Widget>[
+                  const SizedBox(height: 20),
+                  Column(children: <Widget>[
+                    const Text("현재 비밀번호 ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 10),
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              const Center(
+                                child: Text("인증을 위하여 현재 비밀번호를 다시 한번 입력해주세요.",
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                              ),
+                              const SizedBox(height: 10),
+                              Form(
+                                key: _formkey_cur,
+                                child: TextFormField(
+                                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                                  obscureText: true,
+                                  keyboardType: TextInputType.text,
+                                  controller: cur, //기본으로 자판 모양의 키보드 호출되도록 설정
+                                  decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: pwc,
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: const BorderSide(width: 0,)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: const BorderSide(
+                                            width: 2, color: Color(0xffFFB877))),
+                                  ),
+                                  focusNode: _pwtextFieldFocus,
+                                  onSaved: (text) {
+                                    setState(() {
+                                      curpw = text as String; //텍스트 필드가 변할 때 마다 그 값을 저장하도록 설정
+                                    });
+                                  },
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Please enter Current Password";
+                                    }
+                                    return null;
+                                  },
+                                  onChanged: (text) {
+                                    if (_formkey_cur.currentState!.validate()) {
+                                      setState((){
+                                        check = true;
+                                      });
+                                    } else {
+                                      setState(() {
+                                        check = false;
+                                      });
+                                    }
+                                  },
+                                ),
+                              ),
+                              Center(
+                                child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
+                                    onPressed: () async {
+                                      if (check == true){
+                                        if (await check_curPW(id!, cur.value.text) == true){
+                                          Get.to(() => ChangePW(),);
+                                        }
+                                        else{
+                                          displayDialog_checkonly(context, "비밀번호 오류", "비밀번호를 잘못 입력하셨습니다.\n다시 시도해주십시오.");
+                                        }
+                                      }
+                                      else{
+                                        displayDialog_checkonly(context, "비밀번호 오류", "비밀번호를 입력해주십시오.");
+                                      }
+                                    },
+                                    child: const Text("다음", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))),
+                              )
+                            ])
+                    ),
+                            ])
+                ])
+            )
+        ));
+  }
+}
