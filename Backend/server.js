2d93b9f8eaba5228fcd7cde957b68730f581b01a
Ryumaker <sw4968@naver.com>
2023-01-25 22:32:06 +0900

    Add nodemailer

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const schedule = require('node-schedule');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(bodyParser.json({extended: true}));
