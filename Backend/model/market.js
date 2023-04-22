9d46f8edf656597a2c9ba5dd80c5dd45b2adbfd0
Ryumaker <sw4968@naver.com>
2023-04-23 00:45:49 +0900

    Modify SQL in market part

--- Backend/model/market.js
+++ Backend/model/market.js
@@ -3,18 +3,22 @@ const connection = require('../config/database.js');
 module.exports = {
     getSearch: function (keyword, callback) {
         const sql = "\
-        SELECT token_id, product_name, owner, place, performance_date, seat_class, seat_No \
+        SELECT T1.token_id, T1.product_name, T1.owner, T1.place, T1.performance_date, T1.seat_class, T1.seat_No, T2.poster_url \
+        FROM (SELECT token_id, product_name, owner, place, performance_date, seat_class, seat_No \
         FROM user_db.auction_tickets NATURAL JOIN user_db.tickets \
         WHERE product_name LIKE (?) \
-        AND now() < auction_end_date;";
-        connection.query(sql, [`%${keyword}%`], callback);
+        AND now() < auction_end_date \
+        ORDER BY seat_class DESC, seat_No ASC) T1 LEFT JOIN user_db.ticket_description T2 \
+        ON (T1.product_name = T2.product_name);";
+        connection.query(sql, [`%${keyword}%`], callback); // 2차 티켓 중 이름에 keyword가 들어간 티켓을 가져오기
     },
 
     getAuctionInfo: function (token_id, callback) {
         const sql = "\
         SELECT * \
         FROM user_db.auction_tickets \
-        WHERE token_id = (?);";
+        WHERE token_id = (?); \
+        AND now() < auction_end_date;";
         connection.query(sql, [token_id], callback);
     },
 
@@ -55,35 +59,41 @@ module.exports = {
 
     getTop5RankBid: function (callback) {
         const sql = "\
-        SELECT T.token_id, T.product_name, T.owner, T.place, T.performance_date, T.seat_class, T.seat_No, count(*) as `bid_count` \
+        SELECT T1.token_id, T1.product_name, T1.owner, T1.place, T1.performance_date, T1.seat_class, T1.seat_No, T1.bid_count, T2.poster_url \
+        FROM (SELECT T.token_id, T.product_name, T.owner, T.place, T.performance_date, T.seat_class, T.seat_No, count(*) as `bid_count` \
         FROM user_db.auction A LEFT JOIN user_db.tickets T \
         ON (A.token_id = T.token_id) \
         GROUP BY A.token_id \
         ORDER BY `bid_count` DESC, product_name ASC, place ASC \
-        LIMIT 0, 5;";
+        LIMIT 0, 5) T1 LEFT JOIN user_db.ticket_description T2 \
+        ON (T1.product_name = T2.product_name);";
         connection.query(sql, callback);
     },
 
     get5DeadlineAuction: function (callback) {
         const sql = "\
-        SELECT product_name, place, seat_class, seat_No \
+        SELECT T1.token_id, T1.product_name, T1.owner, T1.place, T1.seat_class, T1.seat_No, T1.auction_end_date, T2.poster_url \
+        FROM (SELECT token_id, product_name, owner, place, seat_class, seat_No, auction_end_date \
         FROM user_db.auction_tickets NATURAL JOIN user_db.tickets \
         WHERE now() < auction_end_date \
         AND now() >= DATE_SUB(auction_end_date, INTERVAL 1 DAY) \
-        GROUP BY product_name, place \
-        ORDER BY product_name ASC, place ASC \
+        GROUP BY product_name, place, token_id \
+        ORDER BY product_name ASC, place ASC) T1 LEFT JOIN user_db.ticket_description T2 \
+        ON (T1.product_name = T2.product_name) \
         LIMIT 0, 5;";
         connection.query(sql, callback);
     },
 
     getAllDeadlineAuction: function (callback) {
         const sql = "\
-        SELECT product_name, place, seat_class, seat_No \
+        SELECT T1.token_id, T1.product_name, T1.owner, T1.place, T1.seat_class, T1.seat_No, T1.auction_end_date, T2.poster_url \
+        FROM (SELECT token_id, product_name, owner, place, seat_class, seat_No, auction_end_date \
         FROM user_db.auction_tickets NATURAL JOIN user_db.tickets \
         WHERE now() < auction_end_date \
         AND now() >= DATE_SUB(auction_end_date, INTERVAL 1 DAY) \
-        GROUP BY product_name, place \
-        ORDER BY product_name ASC, place ASC";
+        GROUP BY product_name, place, token_id \
+        ORDER BY product_name ASC, place ASC) T1 LEFT JOIN user_db.ticket_description T2 \
+        ON (T1.product_name = T2.product_name)";
         connection.query(sql, callback);
     },
 
