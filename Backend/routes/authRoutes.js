bfac8f0a95b90e9d7665c540463d98196999f95d
Ryumaker <sw4968@naver.com>
2023-05-12 18:46:04 +0900

    Rewrite codes by separating the routes in authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controller/authController");
 
-router.post('/login', authController.login);
-router.get('/data', authController.home);
-router.post('/signup', authController.signup);
-router.post('/updatePassword', authController.updatePassword);
-router.post('/updateNickname', authController.updateNickname);
-router.post('/checkNickname', authController.checkNickname);
-router.post('/checkPassword', authController.checkPassword);
-router.post('/kas_address', authController.kasAddress);
+const signup = require("../controller/auth/signup");
+const login = require("../controller/auth/login");
+const home = require("../controller/auth/home");
+const updatePassword = require("../controller/auth/updatePassword");
+const updateNickname = require("../controller/auth/updateNickname");
+const checkNickname = require("../controller/auth/checkNickname");
+const checkPassword = require("../controller/auth/checkPassword");
+const kasAddress = require("../controller/auth/kasAddress");
+const sendEmail = require("../controller/auth/sendEmail");
+const sendRandomPW = require("../controller/auth/sendRandomPW");
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/data', home);
+router.post('/updatePassword', updatePassword);
+router.post('/updateNickname', updateNickname);
+router.post('/checkNickname', checkNickname);
+router.post('/checkPassword', checkPassword);
+router.post('/kas_address', kasAddress);
+router.post('/sendEmail', sendEmail);
+router.post('/sendRandomPW', sendRandomPW);
 
 module.exports = router;
