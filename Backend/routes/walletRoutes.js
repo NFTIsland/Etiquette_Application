1431df495c5a2f6dc1932f5545cdd221f4612e12
Ryumaker <sw4968@naver.com>
2023-05-22 21:58:50 +0900

    Rewrite codes by separating the routes in walletRoutes.js

--- Backend/routes/walletRoutes.js
+++ Backend/routes/walletRoutes.js
@@ -3,18 +3,17 @@ const router = express.Router();
 
 const createAccount = require('../controller/kas/wallet/createAccount');
 const checkAccount = require('../controller/kas/wallet/checkAccount');
-const deleteAccount = require('../controller/kas/wallet/deleteAccount');
 const getBalance = require('../controller/kas/wallet/getBalance');
 const transactionRetrieve = require('../controller/kas/wallet/transactionRetrieve');
 const klayTransaction = require('../controller/kas/wallet/klayTransaction');
+const transactionHistory = require('../controller/kas/wallet/transactionHistory');
 
 router.post('/createAccount', createAccount);
 router.get('/checkAccount/', checkAccount);
 router.get('/checkAccount/:address', checkAccount);
-router.delete('/deleteAccount/', deleteAccount);
-router.delete('/deleteAccount/:address', deleteAccount);
 router.post('/getBalance', getBalance);
 router.get('/transactionRetrieve/:transactionHash', transactionRetrieve);
 router.post('/klayTransaction', klayTransaction);
+router.post('/transactionHistory', transactionHistory);
 
 module.exports = router;
