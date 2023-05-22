7ef99117fd890279e1934faf64d954d634909fb6
Ryumaker <sw4968@naver.com>
2023-05-22 21:19:50 +0900

    Rewrite codes by separating the routes in marketRoutes.js

--- Backend/routes/marketRoutes.js
+++ Backend/routes/marketRoutes.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const marketController = require("../controller/marketController");
 
-router.get('/search/:keyword', marketController.search);
-router.post('/auctionInfo', marketController.auctionInfo);
-router.post('/setTicketToBid', marketController.setTicketToBid);
-router.post('/bid', marketController.bid);
-router.post('/bidStatus', marketController.bidStatus);
-router.post('/bidStatusTop5', marketController.bidStatusTop5);
-router.get('/top5RankBid', marketController.top5RankBid);
-router.get('/deadLineTop5Auction', marketController.deadLineTop5Auction);
-router.get('/deadLineAllAuction', marketController.deadLineAllAuction);
-router.delete('/terminateAuction', marketController.terminateAuction);
+const search = require("../controller/market/search");
+const auctionInfo = require("../controller/market/auctionInfo");
+const setTicketToBid = require("../controller/market/setTicketToBid");
+const bid = require("../controller/market/bid");
+const bidStatus = require("../controller/market/bidStatus");
+const bidStatusTop5 = require("../controller/market/bidStatusTop5");
+const top5RankBid = require("../controller/market/top5RankBid");
+const deadLineTop5Auction = require("../controller/market/deadLineTop5Auction");
+const deadLineAllAuction = require("../controller/market/deadLineAllAuction");
+const terminateAuction = require("../controller/market/terminateAuction");
 
-// const marketGeneralRouter = require("./marketGeneralRoutes");
-// router.use("/general", marketGeneralRouter);
+router.get('/search/:keyword', search);
+router.post('/auctionInfo', auctionInfo);
+router.post('/setTicketToBid', setTicketToBid);
+router.post('/bid', bid);
+router.post('/bidStatus', bidStatus);
+router.post('/bidStatusTop5', bidStatusTop5);
+router.get('/top5RankBid', top5RankBid);
+router.get('/deadLineTop5Auction', deadLineTop5Auction);
+router.get('/deadLineAllAuction', deadLineAllAuction);
+router.delete('/terminateAuction', terminateAuction);
 
 module.exports = router;
