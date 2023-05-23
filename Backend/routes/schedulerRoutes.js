70e8208425964aedc315a955cb25aabb11dd28b6
Ryumaker <sw4968@naver.com>
2023-05-23 12:39:38 +0900

    Delete unnecessary schedulerRoutes.js file

--- Backend/routes/schedulerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const schedulerController = require("../controller/schedulerController");
-
-router.post('/auctionSchedule', schedulerController.auctionSchedule);
-
-module.exports = router;
