d34ddc2fce79b086461d9faca11df11a10af0f82
lspc678 <dltmdqls1945@naver.com>
2023-05-29 13:51:46 +0900

    delete Models/dio_models.dart

--- Etiquette/lib/Models/dio_models.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dio/dio.dart';
-Dio getBasicDio() {
-  var dio = Dio();
-  dio.options.connectTimeout = 5000; // 5 seconds
-  dio.options.receiveTimeout = 3000; // 3 seconds
-  return dio;
-}
