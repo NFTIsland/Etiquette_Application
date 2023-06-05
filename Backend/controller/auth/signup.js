6cb9f8240bfc0b050af9473becad31d8e5885ec3
lspc678 <dltmdqls1945@naver.com>
2023-06-05 19:48:12 +0900

    add logic check duplicated klaytn address

--- Backend/controller/auth/signup.js
+++ Backend/controller/auth/signup.js
@@ -19,13 +19,29 @@ const signup = async function (req, res) {
                             msg: "이미 존재하는 닉네임 입니다."
                         });
                     } else {
-                        Auth.insert(req.body.id, req.body.pw, req.body.email, req.body.nickname, req.body.kas_address, function (err, result) {
-                            if (err) throw err;
-                        });
-                        res.status(201);
-                        res.json({
-                            result: true,
-                            msg: "회원가입이 성공적으로 진행되었습니다."
+                        Auth.selectKasAddress(req.body.kas_address, function (err, row) {
+                            if (row != undefined && row.length) {
+                                res.status(409);
+                                res.json({
+                                    result: false,
+                                    msg: "이미 해당 클레이튼 주소를 사용하는 계정이 존재합니다. 다른 주소를 입력해 주세요."
+                                });
+                            } else {
+                                Auth.insert(
+                                req.body.id,
+                                req.body.pw,
+                                req.body.email,
+                                req.body.nickname,
+                                req.body.kas_address,
+                                function (err, result) {
+                                    if (err) throw err;
+                                });
+                                res.status(201);
+                                res.json({
+                                    result: true,
+                                    msg: "회원가입이 성공적으로 진행되었습니다."
+                                });
+                            }
                         });
                     }
                 })
