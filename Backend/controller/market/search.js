52c72a68404cbc0597aab54f57700f6142ce2065
lspc678 <dltmdqls1945@naver.com>
2023-06-05 20:15:47 +0900

    modify market/search.js (add one more logic for empty results)

--- Backend/controller/market/search.js
+++ Backend/controller/market/search.js
@@ -9,13 +9,18 @@ const search = async function (req, res) {
                     statusCode: 200,
                     data: row
                 });
+            } else if (row != undefined) {
+                res.status(201);
+                res.json({
+                    statusCode: 201,
+                    msg: "검색 결과가 없습니다."
+                });
             } else {
                 res.status(402)
                 res.json({
                     statusCode: 402,
                     msg: "경매 티켓 정보를 가져오지 못했습니다. 잠시 후 다시 시도해주세요."
-                });
-                console.log(`market search: ${err}`);
+                });                
             }
         });
     } catch (e) {
